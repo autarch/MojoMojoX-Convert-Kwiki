dac2cb139bb427440e0f65a34c1f57ca48dab398
Dave Rolsky <autarch@urth.org>
2009-07-30 14:09:02 -0500

    add files

---
+++ lib/MojoMojoX/Convert/Kwiki.pm
@@ -0,0 +1,74 @@
+package MojoMojoX::Convert::Kwiki;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+MojoMojoX::Convert::Kwiki - The fantastic new MojoMojoX::Convert::Kwiki!
+
+=head1 SYNOPSIS
+
+XXX - change this!
+
+    use MojoMojoX::Convert::Kwiki;
+
+    my $foo = MojoMojoX::Convert::Kwiki->new();
+
+    ...
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+This class provides the following methods
+
+=head1 AUTHOR
+
+Dave Rolsky, E<gt>autarch@urth.orgE<lt>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-mojomojox-convert-kwiki@rt.cpan.org>,
+or through the web interface at L<http://rt.cpan.org>.  I will be
+notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 DONATIONS
+
+If you'd like to thank me for the work I've done on this module,
+please consider making a "donation" to me via PayPal. I spend a lot of
+free time creating free software, and would appreciate any support
+you'd care to offer.
+
+Please note that B<I am not suggesting that you must do this> in order
+for me to continue working on this particular software. I will
+continue to do so, inasmuch as I have in the past, for as long as it
+interests me.
+
+Similarly, a donation made in this way will probably not make me work
+on this software much more, unless I get so many donations that I can
+consider working on free software full time, which seems unlikely at
+best.
+
+To donate, log into PayPal and send money to autarch@urth.org or use
+the button on this page:
+L<http://www.urth.org/~autarch/fs-donation.html>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Dave Rolsky, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
